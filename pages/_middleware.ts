8cf22e8e2e67ac811d92905971d23ac4b52064a8
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2022-03-08 11:26:46 -0800

    New middleware function

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,54 +1,15 @@
-import { NextURL } from 'next/dist/server/web/next-url';
-import { NextMiddlewareResult } from 'next/dist/server/web/types';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextURL } from "next/dist/server/web/next-url";
+import { NextMiddlewareResult } from "next/dist/server/web/types";
+import { NextResponse, NextRequest } from "next/server";
+
 
 export async function middleware(request: NextRequest): Promise<NextMiddlewareResult> {
-  const cookieKey: string = `bid_${process.env.NEXT_PUBLIC_PARTNER_SANDBOX_CLIENT_KEY}`;
-  const browserId: string = request.cookies[cookieKey];
-  console.log("BrowserId: " + browserId);
-  if (browserId == '' || undefined) {
-    return;
-  }
-  let callFlowResponse = await callFlowsEvent(browserId);
 
-  if (request.url.includes('/home')) {
-    const url: NextURL = request.nextUrl.clone()
 
-    if (callFlowResponse.audienceFilter.includes('personalized')) {
-      url.pathname = '/personalized';
-      return NextResponse.redirect(url);
-    }
-
-    return;
-  }
+    console.log("Url: ", request.url);
+    console.log("Geo: ", request.geo);
+    console.log("IP: ", request.ip);
+    console.log("Cookies: ", request.cookies);
+    return NextResponse.redirect(new NextURL("http://www.google.com"));
 }
 
-async function callFlowsEvent(browserId: string) {
-  const boxeverCallFlowsEndpoint = "https://api.boxever.com/v2/callFlows"
-
-  const payload = {
-    "channel": "WEB",
-    "language": "en",
-    "currencyCode": "EUR",
-    "pointOfSale": "eupos",
-    "browserId": browserId,
-    "clientKey": process.env.NEXT_PUBLIC_PARTNER_SANDBOX_CLIENT_KEY,
-    "friendlyId": "page_view_personalization"
-  }
-
-  const rawResponse = await fetch(boxeverCallFlowsEndpoint, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  });
-
-  if (!rawResponse.ok) {
-    return { segmentCode: '' };
-  }
-
-  const cdpSegmentsResponseJson = await rawResponse.json();
-  console.log("CDP Response:", cdpSegmentsResponseJson.audienceFilter);
-  return cdpSegmentsResponseJson;
-}
